0378509b086eef84d37c6d71a4fa91446abcced0
Nithesh Goutham M <nitheshgoutham.m.2018.ece@rajalakshmi.edu.in>
2024-05-23 22:11:33 +0530

    app.py

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ PROJECTS = {
 st.set_page_config(page_title=PAGE_TITLE, page_icon=PAGE_ICON)
 
 # --- LOAD CSS, PDF & PROFIL PIC ---
-with open("C:\\Users\\Nisha Preetha M\\DIgital_CV\\styles\\main.css") as f:
+with open("https://drive.google.com/file/d/1o5AbKC9oBTgB2MHX9NsaTXvt6BtmFgX5/view?usp=sharing") as f:
     st.markdown("<style>{}</style>".format(f.read()), unsafe_allow_html=True)
 with open("C:\\Users\\Nisha Preetha M\\DIgital_CV\\assets\\CV.pdf", "rb") as pdf_file:
     PDFbyte = pdf_file.read()
