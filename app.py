6092551a39394a2908cb51245d8f6bd6e12ccd4e
Nithesh Goutham M <nitheshgoutham.m.2018.ece@rajalakshmi.edu.in>
2024-05-24 10:52:08 +0530

    app.py

--- app.py
+++ app.py
@@ -6,8 +6,7 @@ import streamlit as st
 
 
 # --- LOAD CSS, PDF & PROFIL PIC ---
-with open(css_file) as f:
-    st.markdown("<style>{}</style>".format(f.read()), unsafe_allow_html=True)
+
 with open(resume_file, "rb") as pdf_file:
     PDFbyte = pdf_file.read()
 profile_pic = Image.open(profile-img)
