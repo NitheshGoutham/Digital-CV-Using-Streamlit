2fbbdf789cae21cf81c18e07d723ed5a9ed0b16c
Nithesh Goutham M <nitheshgoutham.m.2018.ece@rajalakshmi.edu.in>
2024-05-24 11:45:00 +0530

    app.py

--- app.py
+++ app.py
@@ -20,7 +20,6 @@ NAME = "Nithesh Goutham"
 DESCRIPTION = """
 System Engineer, assisting enterprises by supporting data-driven decision-making.
 """
-PDFbyte = get_pdf_bytes('CV.pdf')
 EMAIL = "nitheshgoutham2000@gmail.com"
 resume_file_name = "CV.pdf"
 SOCIAL_MEDIA = {
